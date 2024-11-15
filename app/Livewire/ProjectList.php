ee5efb2c3c17f9a8996d89c529cef7375ce93375
Rich Marisa <rich.marisa@gmail.com>
2024-11-14 21:57:45 -0500

    start projectlist

--- app/Livewire/ProjectList.php
+++ app/Livewire/ProjectList.php
@@ -7,9 +7,68 @@ use App\Models\Project;
 
 class ProjectList extends Component
 {
+    public $category = "All";
+
     public function render()
     {
         $projects = Project::all();
+        $title = "Manage All the Projects";
+
+
+
+            $filtered = $projects->reject(function ($p) use ($a) {
+                if (in_array($p['title'], [
+                    "Support",
+                    "Time Away",
+                    "Drupal Updates",
+                    "Internal Projects and Training",
+                    "Support &#038; Patches &#038; Updates (Drupal &#038; WordPress)"
+                ])) {
+                    return true;
+                }
+
+                if ($category != "All") {
+                    $categories = $p['categories']['nodes'];
+                    foreach ($categories as $c) {
+                        if ($c['name'] == $a['category']) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            })->sortBy("title");
+
+
+
+            $drupal = $projects->where('title', "Drupal Updates")->first();
+            $support = $projects->where('title', "Support &#038; Patches &#038; Updates (Drupal &#038; WordPress)")->first();
+            $timeaway = $projects->where('title', "Time Away")->first();
+            $internal = $projects->where('title', "Internal Projects and Training")->first();
+
+            foreach ($filtered as $p) {
+                $markup .= "<div class='projectarea'>";
+                $markup .= generate_markup_for_project($p);
+                $markup .= "<div><button type='button' class='totalsbutton' data-projectid='{$p["projectId"]}'>Totals</button></div><div class='totals hidden' data-projectid='{$p["projectId"]}'></div>";
+                $markup .= "</div>"; // projectarea
+            }
+
+            $markup .= "<hr/>";
+
+            if ($drupal) {
+                $markup .= generate_markup_for_project($drupal);
+            }
+            if ($support) {
+                $markup .= generate_markup_for_project($support);
+            }
+            if ($timeaway) {
+                $markup .= generate_markup_for_project($timeaway);
+            }
+            if ($internal) {
+                $markup .= generate_markup_for_project($internal);
+            }
+
+
         return view('livewire.project-list', compact('projects'))->layout('layouts.app');
     }
 }
--- resources/views/livewire/project-list.blade.php
+++ resources/views/livewire/project-list.blade.php
@@ -1,5 +1,63 @@
 <div>
+
+       <style>
+        @media screen and (min-width: 1024px) {
+            #content {
+                float: none;
+                width: 90%;
+                margin-left: auto;
+                margin-right:auto;
+            }
+        }
+        #wpadminbar {
+            display: none;
+        }
+        table.persontable td.overcommitted {
+            background-color: red;
+            color: white;
+        }
+        table.persontable td.awaycommitted {
+            background-color: yellow;
+            color: black;
+        }
+        .cell {
+          width: 100px;
+          border-style: solid;
+          border-color: grey;
+
+          font-size:15.2px;
+
+    border-top-width: 0;
+    border-left-width: 0;
+    border-right: 1px solid #ccc;
+    border-bottom: 1px solid #ccc;
+    height: 22px;
+    empty-cells: show;
+          line-height: 21px;
+    padding: 0 4px;
+    background-color: #fff;
+    vertical-align: top;
+    overflow: hidden;
+    outline-width: 0;
+    white-space: pre-line;
+    background-clip: padding-box;
+
+          }
+        .projectrow{
+          max-height: 15px;
+        }
+        .ctr {
+          text-align: center;
+        }
+        .projectnametable {
+          max-width:200px;
+          border-collapse: collapse;
+          flex-shrink: 1;
+        }
+    </style>
+
+
     @foreach ($projects as $project)
-    <h1 style="font-family: Jackwrite, sans-serif; font-size:24pt">{{ $project->name }}</h1> 
+    <h1 style="font-family: Jackwrite, sans-serif; font-size:24pt">{{ $project->name }}</h1>
     @endforeach
 </div>
